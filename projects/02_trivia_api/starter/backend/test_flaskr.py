519ae29bd12c90acff06cad0d0070ddaecb0b73a
Hua Li <lihua.candiac@gmail.com>
2021-02-19 14:51:03 -0500

    add some test cases

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -18,6 +18,7 @@ class TriviaTestCase(unittest.TestCase):
         self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
+        
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
@@ -27,13 +28,62 @@ class TriviaTestCase(unittest.TestCase):
     
     def tearDown(self):
         """Executed after reach test"""
+        
         pass
 
     """
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['total_questions'],36)
+        self.assertTrue(data['categories'])
+        self.assertEqual(len(data['questions']),10)
+    
+    def test_404_sent_requesting_beyond_valid_page(self):
+        res = self.client().get('/questions?page=200')
+        data = json.loads(res.data)
 
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'],'resource not found')
+    
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['categories'])
+        self.assertEqual(len(data['categories']),6)
+    
+    def test_add_question(self):
+        res = self.client().post('/add', json={
+            'question':'testing question',
+            'answer':'testing answer',
+            'category':1,
+            'difficulty':1  
+        })
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'],True)
+        
+
+    def test_delete_question(self):
+        
+        res = self.client().delete('/questions/22')
+        data = json.loads(res.data)
+        question = Question.query.filter(Question.id == 22).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'],22)
+        self.assertTrue(len(data['questions']))
+        self.assertEqual(question, None)
+      
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
